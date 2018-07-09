cbdb5802d180747f6feb12aa7250d6cd72c9c3d2
Jack Dabah <jackydabah@gmail.com>
2018-07-09 06:43:41 +0000

    add header/footre navbar + routes & sections

--- app.js
+++ app.js
@@ -6,11 +6,26 @@ var express = require('express'),
 
 //App Setup
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
 
 //Home Route
 app.get('/', function(req, res) {
    res.render('home'); 
 });
+
+//QUIENES SOMOS ROUTES
+//HISTORIA ROUTE
+app.get('/historia', function(req, res) {
+    res.render('historia');
+});
+app.get('/staff', function(req,res) {
+   res.render('staff'); 
+});
+app.get('/comunidades_amigas', function(req, res) {
+   res.render('comunidades_amigas'); 
+});
+
+
 
 //Server
 app.listen(process.env.PORT, process.env.IP, function(){
